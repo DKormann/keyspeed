fe664ff483a9800ccd9eceb70c0cc1de7c07ca5e
Dominik Kormann <dominikormann@outlook.com>
2023-06-12 20:08:32 +0200

    correnction + mubydick

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,8 @@
     import eng_list from "./eng_list.json"
     import { browser } from "$app/environment";
 
+    import {data} from "./mobydick/en"
+
     var content:string[] = []
     var content_per_level = 10
     var content_size = content_per_level
@@ -13,8 +15,10 @@
 
     var history: [string, number][] = []
 
-
     var caret:HTMLElement
+
+    var alt_pressed = false
+
     if (browser){
 
         if(localStorage.level){
@@ -34,14 +38,42 @@
 
     }
 
-
     var gameover_msg = ""
+
+
+    var last_event_time = time()
+
+    function register_event(type:string){
+        const now = time()
+        console.log(now - last_event_time + type);
+        last_event_time = now
+    }
 
     onMount(()=>{
 
         window.addEventListener("keydown", (e)=>{
+
+            register_event("_D_"+e.code)
+            if (e.key == "'"){
+                e.preventDefault()
+            }
             if (e.key.length==1){
                 type(e.key)
+            }else if (e.key == "Backspace"){
+                if (alt_pressed){
+                    del_word()
+                }else{
+                    del()
+                }
+
+            }else if (e.key == "Alt"){
+                alt_pressed = true
+            }
+        })
+        window.addEventListener("keyup", (e)=>{
+            register_event("_U_"+e.code)
+            if (e.key == "Alt"){
+                alt_pressed = false
             }
         })
         start_game()
@@ -52,7 +84,6 @@
 
     var hunt_started = false
 
-
     function type(key:string){
 
         if (!game_started){
@@ -60,12 +91,17 @@
         }
         if (!hunt_started){
             start_hunt()
+            caret.style.display = "block"
         }
 
         if (content.length == 0){
             return
         }
 
+        if (typed_count > letter_count){
+            return
+        }
+        
         const target_character = typed_letter_count==-1 ? " " : content[typed_word_count][typed_letter_count]
 
         const letter = document.getElementById("L"+typed_word_count+"_"+typed_letter_count)
@@ -75,28 +111,36 @@
 
             caret.style.left = letter!.offsetLeft + letter.offsetWidth - 2 + "px"
             caret.style.top = letter!.offsetTop + 4 + "px"
-        }else{
+        }else{ // space was typed
             var newleft =  Number(caret.style.left.slice(0,-2))
             caret.style.left = newleft + 4 + "px"
         }
             
         
         if (target_character == key){
-
-            const letter = document.getElementById("L"+typed_word_count+"_"+(typed_letter_count))
+            // const letter = document.getElementById("L"+typed_word_count+"_"+(typed_letter_count))
+            letter?.classList.remove("wrong")
             letter?.classList.add("done")
             
         }else{
-            end_game()
-            return
+            letter?.classList.add("wrong")
+            letter?.classList.remove("done")
+            // return
         }
 
-        typed_count += 1
-        if (typed_count>= letter_count){
+
+        if (typed_count +1 >= letter_count){
+            if (document.querySelector(".wrong") != null){
+                // end_game("ERROR")
+                return
+            }
             content_size += content_per_level
             content = []
             end_game("WIN")
             level += 1
+            localStorage.data_line_num = data_line_num
+            localStorage.data_word_num = data_word_num
+
 
             if (level > highscore){
                 highscore = level
@@ -108,8 +152,9 @@
 
             localStorage.level = level
             return
-        }
+        }        
 
+        register_typed(key)
         typed_letter_count += 1
 
         if (typed_letter_count == content[typed_word_count].length){
@@ -119,13 +164,111 @@
     }
 
 
+    function register_typed(key:string){
+
+        typed_count += 1
+        register_event("_A_"+key)
+    }
+
+    function register_delete(n:number=1){
+            
+            typed_count -= n
+            for (let i = 0; i < n; i++) {
+                register_event("_D_")
+            }
+    }
+
+
+    function del(){
+
+        if (typed_letter_count == -1){
+            typed_word_count -= 1
+            typed_letter_count = content[typed_word_count].length - 1
+        }else{
+            typed_letter_count -= 1
+        }
+        const letter = document.getElementById("L"+typed_word_count+"_"+typed_letter_count)
+        letter?.classList.remove("done")
+        letter?.classList.remove("wrong")
+        if (letter != null){
+            caret.style.left = letter!.offsetLeft + "px"
+        }else{ //space deleted
+            const newleft = Number(caret.style.left.slice(0,-2))
+            caret.style.left = newleft - 4 + "px"
+        }
+
+        register_delete()
+    }
+
+    function reset_letter(letter:HTMLElement){
+        letter.classList.remove("done")
+        letter.classList.remove("wrong")
+        letter.classList.add("new")
+    }
+
+    function del_word(){
+
+        if (typed_letter_count>0){
+
+            register_delete(typed_letter_count + 1)
+
+        }
+
+        if (typed_letter_count<=0){
+
+            const target_word = content[typed_word_count-1]
+            if (target_word != undefined){
+
+                register_delete(target_word.length + 1)
+            }
+            
+            
+            if (typed_letter_count == 0){
+                register_delete()
+            }
+
+            typed_word_count -= 1
+        }
+
+
+        typed_letter_count = -1
+
+        if (typed_word_count <= 0){
+            typed_word_count = 0
+            typed_letter_count = 0
+            typed_count = 0
+        }
+
+        const target_word = content[typed_word_count]
+        for (let i = 0; i < target_word.length; i++) {
+            const letter = document.getElementById("L"+typed_word_count+"_"+i)
+            
+            reset_letter(letter!)
+        }
+
+        if(typed_count <0 ){
+            typed_count = 0
+        }
+
+
+        const letter = document.getElementById("L"+typed_word_count+"_"+typed_letter_count)
+
+        if (letter != null){
+            caret.style.left = letter!.offsetLeft + "px"
+        }else{ //space deleted
+            const newleft = Number(caret.style.left.slice(0,-2))
+            caret.style.left = newleft - 4 + "px"
+        }
+    }
+
+
+
     var hunted_words = 0
     var hunted_letters = 0
 
 
     function start_hunt(){
 
-        caret.style.display = "block"
 
         start_time = Date.now()
         
@@ -136,7 +279,7 @@
 
     var huntid:number | null = null
 
-    var time = ()=>{ return Date.now() - start_time}
+    function time  (){ return Date.now() - start_time}
 
     function hunt(){
         
@@ -222,6 +365,7 @@
         }
         
         game_started = true
+        
 
         typed_word_count = 0
         typed_letter_count = 0
@@ -241,7 +385,8 @@
         set_random_state(date+level)
         
         while(letter_count < content_size){
-            const new_word = get_word(date+level+letter_count)
+            // const new_word = get_random_word(date+level+letter_count)
+            const new_word = get_word()
             content.push(new_word)
             letter_count += new_word.length + 1
         }
@@ -269,7 +414,39 @@
         return randomNumber;
     }
 
-    function get_word(seed:string){
+    var data_line_num = 0
+    var data_word_num = 0
+    if (browser){
+
+        if (localStorage.data_line_num){
+            data_line_num = parseInt(localStorage.data_line_num)
+        }
+        if (localStorage.data_word_num){
+            data_word_num = parseInt(localStorage.data_word_num)
+            
+        }
+    }
+    var data_line = data[data_line_num].split(" ")
+
+
+    function get_word(){
+
+        if (data_word_num >= data_line.length){
+
+            
+            data_line_num += 1
+            data_word_num = 0
+            if (data_line_num >= data.length){
+                
+                data_line_num = 0
+            }
+            data_line = data[data_line_num].split(" ")
+        }
+        data_word_num += 1
+        return data_line[data_word_num-1]
+    }
+
+    function get_random_word(seed:string){
 
         const exp = 1000
 
@@ -278,6 +455,13 @@
         idx = Math.floor(idx* eng_list.length)
         const word = eng_list[idx]
         return word
+    }
+
+    var counter=0
+
+    function count(){
+        counter += 1
+        return counter
     }
 
 </script>
@@ -298,16 +482,20 @@
         <span class="hunter">
             {gameover_msg}
         </span>
+        
         {:else}
+            <!-- {(()=>{count = 0;return ""})()} -->
             {#each content as word,i}
                 {#each word as letter,j}
-                    <letter id={"L"+i+"_"+j} class="new">{letter}</letter>
+                    {#if j==0} <span id={"L"+i+"_-1"}>{" "}</span>{/if}<letter id={"L"+i+"_"+j} class="new">{letter}</letter>
+
                 {/each}
-                <letter> </letter>
             {/each}
         {/if}
 
     </h2>
+
+
 
 
     {#if !hunt_started }
